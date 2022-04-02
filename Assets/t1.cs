69bb5222d68afb957d7004a0e150022885e6d287
kawtarRachek <kawtarachek015@gmail.com>
2022-04-02 22:58:41 +0000

    t2

--- Assets/t1.cs
+++ Assets/t1.cs
@@ -9,10 +9,12 @@ public class t1 : MonoBehaviour
 {
     public GameObject player;
     public GameObject obt;
+    public GameObject obt2;
     // Start is called before the first frame update
     void Start()
     {
         obt.SetActive(false);
+        obt2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,10 +23,19 @@ public class t1 : MonoBehaviour
         player.GetComponent<FirstPersonController>().enabled=false;
         obt.SetActive(true);
         StartCoroutine("Ws");
+        
     }
     IEnumerator Ws(){
         yield return new WaitForSeconds(5);
         Destroy(obt);
+        obt2.SetActive(true);
+        StartCoroutine("Ws2");
+        //Destroy(gameObject);
+        //player.GetComponent<FirstPersonController>().enabled=true;
+    }
+    IEnumerator Ws2(){
+        yield return new WaitForSeconds(5);
+        Destroy(obt2);
         Destroy(gameObject);
         player.GetComponent<FirstPersonController>().enabled=true;
     }
