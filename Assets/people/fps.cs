43c2357054ad87c2d17623677e3e568c673bab3d
kawtarRachek <kawtarachek015@gmail.com>
2022-04-05 00:29:15 +0000

    g

--- Assets/people/fps.cs
+++ Assets/people/fps.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.AI; 
+using UnityStandardAssets.Characters.FirstPerson;
 public class fps : MonoBehaviour
 {
+    public GameObject ActionDisplay;
     public GameObject ThePlayer;
+    public GameObject TheCamera;
     // Start is called before the first frame update
     void Start()
     {
         ThePlayer.GetComponent<Animator>().enabled = false;
+        TheCamera.GetComponent<Animator>().enabled = false;
     }
 
-    // Update is called once per frame
+   
     void OnMouseOver()
     {
+        ActionDisplay.SetActive(true);
         if (Input.GetKeyDown(KeyCode.E))
         {
-            ThePlayer.GetComponent<Animator>().Play("FPS1");
+            ThePlayer.GetComponent<Animator>().enabled = true;
+            TheCamera.GetComponent<Animator>().enabled = true;
+            ThePlayer.GetComponent<Animator>().Play("FPS");
+            TheCamera.GetComponent<Animator>().Play("cfps");
+        }
+    }
+    void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+        ThePlayer.GetComponent<Animator>().enabled = false;
+        TheCamera.GetComponent<Animator>().enabled = false;
         }
     }
 }
